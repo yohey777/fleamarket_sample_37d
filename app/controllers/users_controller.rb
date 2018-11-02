b2c854a54bcb1ba4f90dc57fead0fe7facc33a24
Yohei-tutorial <yo.world.traveler.123@gmail.com>
2018-11-02 19:24:36 +0900

    user_controllerの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def registration
+  end
+
+  def phone_authentication
+
+  end
+
+   def registration_completion
+
+  end
+
+   def telephone_registration
+
+  end
+
+end
