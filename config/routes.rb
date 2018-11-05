f031bc265d689114d5f76bdaa201941f268622c5
Yohei-tutorial <yo.world.traveler.123@gmail.com>
2018-11-05 19:52:20 +0900

    route.rbの一部分を削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-   # devise_for :users
    devise_for :users, :controllers => {
     :registrations => 'devise_users/registrations'
    }
@@ -10,9 +9,8 @@ Rails.application.routes.draw do
   resources :items
 
   root 'items#index'
-  #勝手にaddress/newが「post」になるのでgetに変えました
-  get 'address/new' => 'addresses#new'
-  get  'users/sign_up/2'  =>  'users#telephone_registration'
+  get   'address/new' => 'addresses#new'
+  get   'users/sign_up/2'  =>  'users#telephone_registration'
   get   'users/sign_up/3'  =>  'users#phone_authentication'
   get   'users/sign_up/4'   =>  'users#address_input'
   get   'users/sign_up/5'   =>  'users#method_of_payment'
