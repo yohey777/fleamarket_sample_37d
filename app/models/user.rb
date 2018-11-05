4b88e882ea1b417dbd860c7b8339a7577fca2299
Yohei-tutorial <yo.world.traveler.123@gmail.com>
2018-11-05 17:39:14 +0900

    userモデルにアソシエーションを組んだ

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 6 }
+  has_one :address, dependent: :destroy
 
 end
