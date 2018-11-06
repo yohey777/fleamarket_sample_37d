0a099c0604df97a7b537fa03df4c3359e558ab65
Yohei-tutorial <yo.world.traveler.123@gmail.com>
2018-11-06 19:52:30 +0900

    masterからupdateしたファイル
    # Conflicts:
    #	Gemfile
    #	Gemfile.lock

--- db/migrate/20181101114210_create_images.rb
+++ db/migrate/20181101114210_create_images.rb
@@ -2,6 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.1]
   def change
     create_table :images do |t|
       t.string :image_url
+      t.integer :item_id, foreign_key: true
       t.timestamps
     end
   end
