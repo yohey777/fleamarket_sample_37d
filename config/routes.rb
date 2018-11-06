5c518e4c94ed5d7380a4da542608b5c61cc67369
lily_bell514 <lilybell.5555@gmail.com>
2018-11-06 15:55:23 +0900

    Add users rooting

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   resources :items do
     post 'images' => 'images#create'
   end
+
+  get   'address/new' => 'addresses#new'
+  get   'users/sign_up/2'  =>  'users#telephone_registration'
+  get   'users/sign_up/3'  =>  'users#phone_authentication'
+  get   'users/sign_up/4'   =>  'users#address_input'
+  get   'users/sign_up/5'   =>  'users#method_of_payment'
+  get   'users/sign_up/6'   =>  'users#registration_completion'
+
 end
