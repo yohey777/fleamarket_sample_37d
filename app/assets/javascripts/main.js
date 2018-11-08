273953b8cdb89c2162a82686a97876a3ddd2665e
lily_bell514 <lilybell.5555@gmail.com>
2018-11-08 11:57:39 +0900

    Delete dropzone.js and dropzone.css

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -56,22 +56,4 @@ $(function(){
     e.preventDefault();
   });
 });
-// ドロッグアンドドロップ
-Dropzone.autoDiscover = false;
-var headlineDropzone = new Dropzone("#upload-dropzone", {
-  url: "/upload_photo", // You can override url of form in here.
-  maxFilesize: 5, // in MB
-  parallelUploads: 2,
-  acceptedFiles:'.jpg, .png, .jpeg, .gif', // type of files
-  init: function(){
-    this.on('addedfile', function(file) {
-      // Called when a file is added to the list.
-    });
-    this.on('sending', function(file, xhr, formData) {
-      // Called just before each file is sent.
-    });
-    this.on('success', function(file, json) {
-      // Called when file uploaded successfully.
-    });
-  }
-});
+
