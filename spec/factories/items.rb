66cf729d7312e974462ed72e35221d268afcde6a
lily_bell514 <lilybell.5555@gmail.com>
2018-11-09 19:53:36 +0900

    テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
-FactoryBot.define do
+FactoryGirl.define do
   factory :item do
+    name             'ppppppppppppppp'
+    price             1000
+    description       "pppppppppp"
+    status            1
+    prefecture        1
+    expense           1
+    shipping_method   1
+    arrival_date      1
+    L_category_id     1
+    M_category_id     10
+    S_category_id     1
+    like_count        5
+    size_id           1
+    brand_id          1
 
   end
 end
