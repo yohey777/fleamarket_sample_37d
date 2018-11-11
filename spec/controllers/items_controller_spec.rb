89d8ef90d14f4b46a89ccc9ce903548de4fe9314
lily_bell514 <lilybell.5555@gmail.com>
2018-11-11 19:14:30 +0900

    出品時のコントローラーテスト

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe ItemsController, type: :controller do
+describe ItemsController, type: :controller do
+  render_views
+  let(:item){create(:item)}
 
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+   describe 'POST #create' do
+
+    it 'item successfully saved' do
+        item = build(:item)
+        expect{item.save}.to change{Item.count}.by(1)
+        expect(item.save).to be_truthy
+    end
+
+    it 'item not successfully saved' do
+        item = Item.new
+        expect(item.save).to be_falsey
+    end
+  end
 end
